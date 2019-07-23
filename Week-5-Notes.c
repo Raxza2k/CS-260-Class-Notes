1cd5f836b1618acceeaca1ef23ec76773046bff2
Raxza2k <Anthony.Minniti@protonmail.com>
2019-07-23 15:29:31 -0700

    Update week 5 notes.

--- Week-5-Notes.c
+++ Week-5-Notes.c
@@ -10,6 +10,40 @@
         See a diagram of the tree functionally.
         //I see what it means when we say each node is visited three times.
         //However the descriptions are tedious, look at Cooper's notes.
+                           P
+                         S        E
+                       A | M    L |  R
+                          A        T | E
+                                     E |
 
+      Pre-order: psamallrtee
+      In-order: asampletree
+      Post-oreder: aamslteerep
+*/
 
+//Recursive funcion for Euler tour.
+void EulerTour(struct Node* node){
+  if (node != 0){
+    beforeLeft(node);
+    EulerTour(node->left);
+    inBetween(node);
+    EulerTour(node->right);
+    afterRight(node);
+  }
+}
+
+void beforeLeft(Node n) { pfintf("()")};
+void inBetween(Node n) { pfintf("%d\n", val)};
+void afterRight(Node n) { pfintf(")")};
+
+/*
+                        +
+                      a | *
+                        + | d
+                      b | c
+      Pre-Oreder: +a*+bcd       //Polish notation (Prefix notation)
+      In-Order: a + B + c * d   //Regular notation (Infix noation)
+      Post-Order: abc+d*+       //Reverse polish notation (Post fix notation)
+
+      Time complexity of this process is O(n).
 */
