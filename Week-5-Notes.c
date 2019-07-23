3b95504b455d8af426a16b8ef5eb00f31c5a030c
Anthony Minniti <Aminniti@live.com>
2019-07-23 15:12:47 -0700

    Add files via upload

---
+++ Week-5-Notes.c
@@ -0,0 +1,15 @@
+//                             07/23/19
+///////////////////////////////////////////////////////////////////////////////
+/*
+        Euler Tours/Tree Trarerjals
+        What are they, and how are they usefull?
+        The Euler tours is the tracing of the tree, visiting each node 3 times.
+        1) Right side - Pre-order
+        2) Left side - In-order
+        3) Bottom - Post-order
+        See a diagram of the tree functionally.
+        //I see what it means when we say each node is visited three times.
+        //However the descriptions are tedious, look at Cooper's notes.
+
+
+*/
