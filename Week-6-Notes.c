9e69d8987ff914acd54766014f664e3e98261ea4
Raxza2k <Anthony.Minniti@protonmail.com>
2019-07-31 15:49:55 -0700

    This is the Wed. notes update

--- Week-6-Notes.c
+++ Week-6-Notes.c
@@ -65,3 +65,85 @@ Hash function checks 3rd letter of name, and assigns containter.
   ////// HOW DOES THE HASH FUNCTION RELATE TO THE MBR TABLE PARTITION SCHEME?///
   //////// THIS HASH FUNCTION IS REALLY COMPLEX, STUDY IT'S CONSTRUCTION.//////
 */
+
+
+////////////////////////////////////////////////////////////////////////////////
+/*
+                          Buckets(Chaining)
+    In the Open Address Hashing(O.A.H.) we have an array of individual slots
+      that graudually get filled. The load order is closest to desired slot, if
+      desired slot is filled.
+
+    We will be implementing Counting sort and Radix sort to implement this hashing
+      table.
+
+
+    The 1st level of the Hash table array is the buckets.
+    The chains are the arrays tied to each slot of the array.
+
+    The sort uses bucket "size" and remainder to decide location of the data.
+    You could back load or front loat the data. However front loading is O(1)
+      and more efficent.
+*/
+
+struct HashTable{
+    struct LinkedList** table;
+    int capacity;
+    int count
+}
+
+void initHashTable(struct HashTable* ht, int size){
+    int i;
+    ht->capacity = size;
+    ht->count = 0;
+    ht->table = malloc(ht->capacity * sizeof(struct LinkedList));
+    assert(ht->table != 0);
+    for(i = 0; i < ht->capacity; i++){
+        ht->tabe[i] = newList();  //Add a new list pointer to the linked list #include
+                                  //   in each bucket.
+    }
+}
+
+void addHashTable(struct HashTable* ht, TYPE val){
+    int idx = hash(val) % ht->capacity;
+    if(inx < 0){
+      idx += ht->capacity;
+    }
+    addList(ht->table[idx], val);
+    ht->count++;
+    /*Recognize if the load factor is too large*/
+    //Review the lambda calculation and load factor.   <===== IMPORTANT
+}
+
+
+
+/*
+            Counting sort
+
+    What kind of sort is the counting sort?
+
+    When is it used?
+      When you want to count things...
+
+      1) Go through and hash int array.
+      2) Count number of times each number occurs.
+      3) rewrite the array with data from the hash in order.
+*/
+
+void countSort(int data[], int m, int max){
+                           // n - size of data | m - max value in data[]
+     int i,j,k;
+     int* cnt = malloc((max + 1) * sizeof(int));
+     for(i = 0; i < m; i++){   //O(n)
+        cnt[data[i]]++;
+     }
+     i = 0;
+     //This double for loop is complex and I don't really get it.
+     for(j = 0; j <= max; j++){ //O(max value)
+        for(k = cnt[j]; k > 0){ //O(size of data[])
+                                             // =>  O(max * size) -> O(n^2)
+           //This works if you have a lot of small values, in a large array.
+           data[i++] = j;
+        }
+     }
+}
