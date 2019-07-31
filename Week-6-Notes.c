4084fdc4b80c7b7d9fa513a1ec175ffb06f881df
Raxza2k <Anthony.Minniti@protonmail.com>
2019-07-30 20:59:57 -0700

    This is tuesday week 6 notes.

--- Week-6-Notes.c
+++ Week-6-Notes.c
@@ -32,5 +32,36 @@ Hash function checks 3rd letter of name, and assigns containter.
   found. This would put the name in container 6.
 
   When the contains function starts looking for Aimee, it starts at 4, then looks
-  until it finds it. 
+  until it finds it. O(n)!
+
+  When you go to remove a value, you find the value, then empty the container by
+  setting it to NULL.
+  Aimee EX) Go to 4, probe to Aimee, then remove from table.
+  Well, what if you hit NULL before finding a value you're looking for?
+    1) Do not remove value. (meh)
+    2) Special character indicating empty. "tombstone"
+
+
+  The statistic of the hash table is the LOAD FACTOR. It tells us how much of
+  the table is filled.
+
+  lambda = h/m = [number of elements]/[table size]
+
+  The lower the load factor is, the better the serach function time.
+    sucessful searches               unsucessful searches
+    s = 1/2(1+1/(1-3))             u = 1/2(1 + 1/(1 - lambda)^2)
+    =>
+    lambda                  s                          u
+    0.5                    1.5                        2.5
+    0.75                   2.5                        8.5
+    0.9                    5.5                        50.5
+    This graph had an exponential curve. Meaning as utilization goes up in the
+    hash table, we see a decrease in performance.
+
+    Some spots are more likley to be filled than others as utilization goes up.
+    This is due to the way we've constructed the hash function.
+
+
+  ////// HOW DOES THE HASH FUNCTION RELATE TO THE MBR TABLE PARTITION SCHEME?///
+  //////// THIS HASH FUNCTION IS REALLY COMPLEX, STUDY IT'S CONSTRUCTION.//////
 */
